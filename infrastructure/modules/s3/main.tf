0088ecd6fae0246d51d7e7a44f3472adc7651794
Kiran Kumar Arka <arka.kk5@gmail.com>
2026-04-03 22:10:53 +0000

    fixed S3 CORS issue

--- infrastructure/modules/s3/main.tf
+++ infrastructure/modules/s3/main.tf
@@ -12,3 +12,14 @@ resource "aws_s3_bucket_public_access_block" "block" {
   block_public_acls   = true
   block_public_policy = true
 }
+
+resource "aws_s3_bucket_cors_configuration" "cors" {
+  bucket = aws_s3_bucket.app.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "GET", "POST"]
+    allowed_origins = ["https://transcriber.arkadevops.in"]
+    expose_headers  = []
+  }
+}
